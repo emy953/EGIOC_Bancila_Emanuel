5221a4cd8aefc3333639a5230c3b05722950a18f
Emanuel Bancila <bancila.emanuel953@gmail.com>
2022-05-27 16:30:35 +0300

    commit

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    TMP_Text scoreText;
+
+    void Start() 
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start!\nClick to fire, Use keyboard to move";
+    }
+
+    public void IncreaseScore(int amountToIncrease)
+    {
+        score += amountToIncrease;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+}
